404d8d71e06d3996df0c4dc8584220ebd5d433d0
Aydan Amorim <aydanamorim@gmail.com>
2024-03-23 23:36:10 -0300

    Feat/restaurant (#12)
    * rebase com a origin/developer tentativa de unificar os comandos no github
    
    * Classes para uso do Swagger; Classes Úteis para lançar validações, DTO e paginação para ser usada nas controllers;
    Inclusão das Classes para Inclusão, Alteração e busca de Restaurantes e horário de funcionamento de restaurantes
    
    * Mudança na arquitetura e preparação para a arquitetura limpa
    
    * Implementação de arquitetura limpa na mesma estrutura da branch da Isa e refatoração das classes de restaurant
    também deixei no padrão de tipo de comida usado pelo Danilo, não implementei a parte de endereços

---
+++ src/main/java/com/restaurant/reservationreview/util/converter/RestaurantBusinessHoursConverter.java
@@ -0,0 +1,36 @@
+package com.restaurant.reservationreview.util.converter;
+
+import com.restaurant.reservationreview.interfaceadapters.presenters.dto.RestaurantBusinessHoursDto;
+import com.restaurant.reservationreview.entities.RestaurantBusinessHours;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@Component
+public class RestaurantBusinessHoursConverter implements Converter<RestaurantBusinessHours, RestaurantBusinessHoursDto> {
+
+    @Override
+    public RestaurantBusinessHoursDto convert(RestaurantBusinessHours document){
+
+        RestaurantBusinessHoursDto dto = new RestaurantBusinessHoursDto();
+
+        dto.setDayOfWeek(document.getDayOfWeek());
+        dto.setStartTime(document.getStartTime().toString());
+        dto.setFinishTime(document.getFinishTime().toString());
+
+        return dto;
+    }
+
+    @Override
+    public RestaurantBusinessHours convert(RestaurantBusinessHoursDto dto){
+        RestaurantBusinessHours restaurantBusinessHours = new RestaurantBusinessHours();
+
+        restaurantBusinessHours.setDayOfWeek(dto.getDayOfWeek());
+        restaurantBusinessHours.setStartTime(LocalTime.parse(dto.getStartTime(), DateTimeFormatter.ISO_LOCAL_TIME));
+        restaurantBusinessHours.setFinishTime(LocalTime.parse(dto.getFinishTime(), DateTimeFormatter.ISO_LOCAL_TIME));
+
+        return restaurantBusinessHours;
+    }
+
+}
