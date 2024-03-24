9a494b07ae43abd24962e5cb3c487b856b0c9da9
Aydan Amorim <aydanamorim@gmail.com>
2024-03-23 23:36:10 -0300

    Feat/restaurant (#12)
    * rebase com a origin/developer tentativa de unificar os comandos no github
    
    * Classes para uso do Swagger; Classes Úteis para lançar validações, DTO e paginação para ser usada nas controllers;
    Inclusão das Classes para Inclusão, Alteração e busca de Restaurantes e horário de funcionamento de restaurantes
    
    * Mudança na arquitetura e preparação para a arquitetura limpa
    
    * Implementação de arquitetura limpa na mesma estrutura da branch da Isa e refatoração das classes de restaurant
    também deixei no padrão de tipo de comida usado pelo Danilo, não implementei a parte de endereços

--- src/main/java/com/restaurant/reservationreview/interfaceadapters/presenters/dto/RestaurantDto.java
+++ src/main/java/com/restaurant/reservationreview/interfaceadapters/presenters/dto/RestaurantDto.java
@@ -1,15 +1,46 @@
 package com.restaurant.reservationreview.interfaceadapters.presenters.dto;
 
+import com.restaurant.reservationreview.util.enums.FoodType;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@NoArgsConstructor
-@Setter
+import java.util.List;
+
 @Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class RestaurantDto extends Dto {
 
     public RestaurantDto(String id) {
         super(id);
     }
+
+    @NotBlank(message = "O campo Nome é obrigatório")
+    @Schema(example = "Restaurante Comida Gostosa é aqui")
+    private String name;
+
+    @NotBlank(message = "O campo Localização é obrigatório")
+    @Schema(example = "São Paulo")
+    private String location;
+
+    @NotBlank(message = "O campo Tipo de Cozinha é obrigatório")
+    @Schema(example = "BRAZILIAN")
+    private FoodType foodType;
+
+    private Boolean active;
+
+    private Integer averageRating;
+
+    @NotBlank(message = "O campo Quantidade de Mesas é obrigatório")
+    @Schema(example = "20")
+    private Integer quantityTables;
+
+    private List<RestaurantBusinessHoursDto> businessHoursDto;
+
 }
---
+++ src/main/java/com/restaurant/reservationreview/util/enums/FoodType.java
@@ -0,0 +1,7 @@
+package com.restaurant.reservationreview.util.enums;
+
+public enum FoodType {
+
+    BRAZILIAN, FRENCH, ITALIAN, INDIAN, OTHER
+
+}
