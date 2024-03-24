dada28d40069202dffedc21f1815e1376853f8e0
Danilo Faccio <facciodrums@gmail.com>
2024-03-24 13:38:08 -0300

    ajustes de validação de horário

--- src/main/java/com/restaurant/reservationreview/frameworks/db/ReservationRepository.java
+++ src/main/java/com/restaurant/reservationreview/frameworks/db/ReservationRepository.java
@@ -1,20 +1,17 @@
 package com.restaurant.reservationreview.frameworks.db;
 
 import com.restaurant.reservationreview.entities.Reservation;
-import com.restaurant.reservationreview.entities.Restaurant;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ReservationRepository extends MongoRepository<Reservation, String> {
 
     @Query(value="{'person.email':{$eq:?0}}")
-    Reservation findByEmail(String email);
+    Page<Reservation> findAllByEmail(String email, Pageable page);
 
     Reservation insert(Reservation reservation);
 
---
+++ src/main/java/com/restaurant/reservationreview/interfaceadapters/presenters/ReservationPresenter.java
@@ -0,0 +1,40 @@
+package com.restaurant.reservationreview.interfaceadapters.presenters;
+
+import com.restaurant.reservationreview.entities.Reservation;
+import com.restaurant.reservationreview.interfaceadapters.presenters.dto.ReservationDto;
+import jakarta.annotation.Resource;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ReservationPresenter implements Presenter<Reservation, ReservationDto> {
+
+    @Resource
+    private RestaurantPresenter restaurantPresenter;
+
+    @Resource
+    private PersonPresenter personPresenter;
+
+    @Override
+    public ReservationDto convert(Reservation document) {
+        return new ReservationDto(
+                document.getId(),
+                personPresenter.convert(document.getPerson()),
+                document.getDateAndTime(),
+                document.getDayOfWeek(),
+                document.getReservationAmount());
+    }
+
+    @Override
+    public Reservation convert(ReservationDto dto) {
+        return new Reservation(
+                dto.getId(),
+                personPresenter.convert(dto.getPersonDto()),
+                dto.getDateAndTime(),
+                dto.getDayOfWeek(),
+                dto.getReservationAmount());
+
+    }
+
+}
