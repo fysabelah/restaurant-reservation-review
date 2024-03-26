a8a743b5234192bb8ea269cd0178e73e267050b1
Isabela França <fysabelah@gmail.com>
2024-03-26 09:09:49 -0300

    Alterar cadastro de avalição para atualizar média do restaurante a cada novo cadastro.

--- src/main/java/com/restaurant/reservationreview/frameworks/web/RestaurantWeb.java
+++ src/main/java/com/restaurant/reservationreview/frameworks/web/RestaurantWeb.java
@@ -15,16 +15,16 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/restaurant")
-@Tag(name="Restaurant", description = "Exibe os métodos para consultar e incluir informações de um restaurante")
-
+@Tag(name = "Restaurant", description = "Exibe os métodos para consultar e incluir informações de um restaurante")
 public class RestaurantWeb {
+
     @Resource
     private RestaurantController restaurantController;
 
     @Operation(summary = "Consultar todos os restaurantes")
     @GetMapping
-    public ResponseEntity<PagedResponse<RestaurantDto>> findAll(    @Parameter(description = "Default value 10. Max value 1000", example = "10") @RequestParam(required = false) Integer pageSize,
-                                                                    @Parameter(description = "Default value 0", example = "0") @RequestParam(required = false) Integer initialPage) {
+    public ResponseEntity<PagedResponse<RestaurantDto>> findAll(@Parameter(description = "Default value 10. Max value 1000", example = "10") @RequestParam(required = false) Integer pageSize,
+                                                                @Parameter(description = "Default value 0", example = "0") @RequestParam(required = false) Integer initialPage) {
 
         Pagination page = new Pagination(initialPage, pageSize);
 
@@ -34,15 +34,15 @@ public class RestaurantWeb {
     @Operation(summary = "Consultar um restaurante por código")
     @GetMapping(value = "/{restaurant}")
     public ResponseEntity<RestaurantDto> findById(@Parameter(description = "Informe o ID do restaurante", example = "65fda06d0f8a6b46cd1b2eba")
-                                                      @RequestParam(required = true) String idRestaurant) throws ValidationsException {
+                                                  @RequestParam(required = true) String idRestaurant) throws ValidationsException {
 
         return ResponseEntity.ok(this.restaurantController.findById(idRestaurant));
 
     }
 
-    @Operation(summary="Incluir informações de um restaurante")
+    @Operation(summary = "Incluir informações de um restaurante")
     @PostMapping
-    public ResponseEntity<RestaurantDto>  insert(@RequestBody RestaurantDto restaurantDto) throws ValidationsException {
+    public ResponseEntity<RestaurantDto> insert(@RequestBody RestaurantDto restaurantDto) throws ValidationsException {
 
         RestaurantDto restaurantDtoSaved = this.restaurantController.insert(restaurantDto);
         return ResponseEntity.ok(restaurantDtoSaved);
